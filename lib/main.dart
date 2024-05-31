f360445b72b3790493690a9fd08b60356d74b461
Jonathanwjy <jonathanwijaya05@gmail.com>
2024-05-31 11:48:15 +0700

    merge

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:red_wine/controller/app_user.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-
 import 'package:red_wine/screens/sign_in_screen.dart';
 
 void main() async {
