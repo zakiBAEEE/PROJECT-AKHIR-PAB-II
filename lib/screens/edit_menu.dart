44ce6ae85814c2dd67d2289a17bb8e8ca2638c1a
Jonathanwjy <jonathanwijaya05@gmail.com>
2024-06-01 23:32:40 +0700

    perbaikan edit menu

--- lib/screens/edit_menu.dart
+++ lib/screens/edit_menu.dart
@@ -11,6 +11,12 @@ class _EditMenuState extends State<EditMenu> {
   final TextEditingController _namaController = TextEditingController();
   final TextEditingController _hargaController = TextEditingController();
   final TextEditingController _deskripsiController = TextEditingController();
+  final TextEditingController _jenisController = TextEditingController();
+  final TextEditingController _kategoriController = TextEditingController();
+  final TextEditingController _tokoController = TextEditingController();
+  final ValueNotifier<bool> _isFavorite = ValueNotifier<bool>(false);
+  final ValueNotifier<bool> _isPromo = ValueNotifier<bool>(false);
+  final TextEditingController _jamBuka = TextEditingController();
   String? _imageUrl;
 
   @override
@@ -115,6 +121,167 @@ class _EditMenuState extends State<EditMenu> {
                   controller: _deskripsiController,
                   decoration: InputDecoration(
                     labelText: 'Deskripsi',
+                    border: const OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 16, bottom: 8),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Jenis',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: TextFormField(
+                  controller: _jenisController,
+                  decoration: InputDecoration(
+                    labelText: 'Jenis',
+                    border: const OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 16, bottom: 8),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Kategori',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: TextFormField(
+                  controller: _kategoriController,
+                  decoration: InputDecoration(
+                    labelText: 'Kategori',
+                    border: const OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 16, bottom: 8),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Toko',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: TextFormField(
+                  controller: _tokoController,
+                  decoration: InputDecoration(
+                    labelText: 'Toko',
+                    border: const OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 16, bottom: 8),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Favorite?',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+                            Column(
+                children: <Widget>[
+                  ValueListenableBuilder<bool>(
+                    valueListenable: _isFavorite, 
+                    builder: (context,value, child){
+                      return DropdownButton(
+                          value: value,
+                          onChanged: (bool? newValue){
+                            _isFavorite.value = newValue!;
+                          },
+                          items: const [
+                            DropdownMenuItem(
+                              value: true,
+                              child: Text("True"),
+                            ),
+                            DropdownMenuItem(
+                              value: false,
+                              child: Text("false"),
+                            )
+                          ],
+                        );
+                    })
+                ],
+              ),
+              
+              const Padding(
+                padding: EdgeInsets.only(left: 16, bottom: 8),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Promo?',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Column(
+                children: <Widget>[
+                  ValueListenableBuilder<bool>(
+                    valueListenable: _isPromo, 
+                    builder: (context,value, child){
+                      return DropdownButton(
+                          value: value,
+                          onChanged: (bool? newValue){
+                            _isPromo.value = newValue!;
+                          },
+                          items: const [
+                            DropdownMenuItem(
+                              value: true,
+                              child: Text("True"),
+                            ),
+                            DropdownMenuItem(
+                              value: false,
+                              child: Text("false"),
+                            )
+                          ],
+                        );
+                    })
+                ],
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 16, bottom: 8),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Jam Buka',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: TextFormField(
+                  controller: _jamBuka,
+                  decoration: InputDecoration(
+                    labelText: 'Jam Buka',
                     border: const OutlineInputBorder(),
                   ),
                 ),
