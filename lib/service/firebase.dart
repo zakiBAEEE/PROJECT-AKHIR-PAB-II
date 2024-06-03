b334111d92d1c805c5ed5d00392862436c3203ae
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2024-06-03 09:04:02 +0700

    Membuat service addProduk

--- lib/service/firebase.dart
+++ lib/service/firebase.dart
@@ -1,4 +1,3 @@
-
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -10,17 +9,23 @@ import 'package:red_wine/models/menu.dart';
 import 'package:red_wine/models/user.dart';
 import 'package:path/path.dart' as path;
 
-
 class MenuService {
   static final FirebaseFirestore _database = FirebaseFirestore.instance;
-  
-  static final CollectionReference _userCollection = _database.collection('toko');
+
+  static final CollectionReference _userCollection =
+      _database.collection('toko');
 
   static final FirebaseStorage _storage = FirebaseStorage.instance;
 
 // ========================================================================================
-  static Stream<List<Komentar>> getKomentarList(String produkId, String tokoId) {
-    return _userCollection.doc(tokoId).collection('produk').doc(produkId).collection('komentar').orderBy('created_at', descending: true)
+  static Stream<List<Komentar>> getKomentarList(
+      String produkId, String tokoId) {
+    return _userCollection
+        .doc(tokoId)
+        .collection('produk')
+        .doc(produkId)
+        .collection('komentar')
+        .orderBy('created_at', descending: true)
         .snapshots()
         .map((snapshot) {
       return snapshot.docs.map((doc) {
@@ -35,101 +40,135 @@ class MenuService {
       }).toList();
     });
   }
- static Future<void> addKomentar(String komen, String produkId, String tokoId, String namaPengguna, String imageUrl, String idUser) async {
+
+  static Future<void> addKomentar(String komen, String produkId, String tokoId,
+      String namaPengguna, String imageUrl, String idUser) async {
     Map<String, dynamic> newKomen = {
       'komentar': komen,
       'namaPengguna': namaPengguna,
-      'imageUrl' : imageUrl,
-      'idUser' : idUser,
+      'imageUrl': imageUrl,
+      'idUser': idUser,
       'created_at': FieldValue.serverTimestamp(),
       'update_at': FieldValue.serverTimestamp(),
     };
-    await _userCollection.doc(tokoId).collection('produk').doc(produkId).collection('komentar').add(newKomen);
+    await _userCollection
+        .doc(tokoId)
+        .collection('produk')
+        .doc(produkId)
+        .collection('komentar')
+        .add(newKomen);
   }
 
-
-
-    static Future<void> updateKomentar(Komentar komentar, String tokoId, String produkId, String newKomen,) async {
+  static Future<void> updateKomentar(
+    Komentar komentar,
+    String tokoId,
+    String produkId,
+    String newKomen,
+  ) async {
     Map<String, dynamic> updatedNote = {
-      'komentar' : newKomen,
+      'komentar': newKomen,
       'created_at': komentar.createdAt,
       'updated_at': FieldValue.serverTimestamp(),
     };
 
-      await _userCollection.doc(tokoId).collection('produk').doc(produkId).collection('komentar').doc(komentar.id).update(updatedNote);
+    await _userCollection
+        .doc(tokoId)
+        .collection('produk')
+        .doc(produkId)
+        .collection('komentar')
+        .doc(komentar.id)
+        .update(updatedNote);
   }
 
-   static Future<void> deleteKomentar(Komentar komentar, String tokoId, String produkId, ) async {
-     await _userCollection.doc(tokoId).collection('produk').doc(produkId).collection('komentar').doc(komentar.id).delete();
+  static Future<void> deleteKomentar(
+    Komentar komentar,
+    String tokoId,
+    String produkId,
+  ) async {
+    await _userCollection
+        .doc(tokoId)
+        .collection('produk')
+        .doc(produkId)
+        .collection('komentar')
+        .doc(komentar.id)
+        .delete();
   }
 // =====================================================================
 
+  static Stream<List<Menu>> getProdukList() {
+    return _userCollection.snapshots().asyncMap((snapshot) async {
+      List<Menu> menuList = [];
+      for (var doc in snapshot.docs) {
+        QuerySnapshot menuSnapshot =
+            await doc.reference.collection('produk').get();
+        String idToko = doc.id;
+        List<Menu> menus = menuSnapshot.docs.map((menuDoc) {
+          Map<String, dynamic> data = menuDoc.data() as Map<String, dynamic>;
+          return Menu(
+            idToko: idToko,
+            id: menuDoc.id,
+            title: data['title'],
+            description: data['description'],
+            imageUrl: data['imageUrl'],
+            createdAt: data['created_at'] != null
+                ? data['created_at'] as Timestamp
+                : null,
+            updateAt: data['updated_at'] != null
+                ? data['updated_at'] as Timestamp
+                : null,
+            harga: data['harga'],
+            jenis: data['jenis'],
+            kategori: data['kategori'],
+            isFavorite: data['isFavorite'],
+            isPromo: data['isPromo'],
+            jamBuka: data['jamBuka'],
+            toko: data['toko'],
+          );
+        }).toList();
+        menuList.addAll(menus);
+      }
+      return menuList;
+    });
+  }
 
-static Stream<List<Menu>> getProdukList() {
-  return _userCollection.snapshots().asyncMap((snapshot) async {
-    List<Menu> menuList = [];
-    for (var doc in snapshot.docs) {
-      QuerySnapshot menuSnapshot = await doc.reference.collection('produk').get();
-      String idToko = doc.id;
-      List<Menu> menus = menuSnapshot.docs.map((menuDoc) {
-        Map<String, dynamic> data = menuDoc.data() as Map<String, dynamic>;
-        return Menu(
-          idToko: idToko,
-          id: menuDoc.id,
-          title: data['title'],
-          description: data['description'],
-          imageUrl: data['imageUrl'],
-          createdAt: data['created_at'] != null ? data['created_at'] as Timestamp : null,
-          updateAt: data['updated_at'] != null ? data['updated_at'] as Timestamp : null,
-          harga: data['harga'],
-          jenis: data['jenis'],
-          kategori: data['kategori'],
-          isFavorite: data['isFavorite'],
-          isPromo: data['isPromo'],
-          jamBuka: data['jamBuka'],
-          toko: data['toko'],
-        );
-      }).toList();
-      menuList.addAll(menus);
-    }
-    return menuList;
-  });
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  static Future<void> addProduk(String tokoId, Menu menu) async {
+    Map<String, dynamic> newProduk = {
+      'title': menu.title,
+      'description': menu.description,
+      'imageUrl': menu.imageUrl,
+      'harga': menu.harga,
+      'isFavorite': false,
+      'isPromo': menu.isPromo,
+      'jamBuka': menu.jamBuka,
+      'jenis': menu.jenis,
+      'kategori': menu.kategori,
+      'toko': menu.toko,
+      'created_at': FieldValue.serverTimestamp(),
+      'update_at': FieldValue.serverTimestamp(),
+    };
+    await _userCollection.doc(tokoId).collection('produk').add(newProduk);
+  }
 
 // ==================================================================================
-static Future<void> addUser(String idUser, String nama, String email, String jenisUser) async {
-
+  static Future<void> addUser(
+      String idUser, String nama, String email, String jenisUser) async {
     Map<String, dynamic> newUser = {
-      'idUser' : idUser,
+      'idUser': idUser,
       'nama': nama,
-      'email' : email,
-      'jenisUser' : jenisUser,
-      'imageUrl' : "",
+      'email': email,
+      'jenisUser': jenisUser,
+      'imageUrl': "",
       'created_at': FieldValue.serverTimestamp(),
       'update_at': FieldValue.serverTimestamp(),
     };
     await _userCollection.add(newUser);
   }
 
-static Stream<Pengguna> getUser(String idUser) {
-  return _userCollection
-      .where('idUser', isEqualTo: idUser)
-      .snapshots()
-      .map((snapshot) {
+  static Stream<Pengguna> getUser(String idUser) {
+    return _userCollection
+        .where('idUser', isEqualTo: idUser)
+        .snapshots()
+        .map((snapshot) {
       var doc = snapshot.docs.first;
       Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
       return Pengguna(
@@ -139,45 +178,44 @@ static Stream<Pengguna> getUser(String idUser) {
         imageUrl: data['imageUrl'],
         jenisUser: data['jenisUser'],
       );
-  });
-}
-
- static Future<void> updateUser(Pengguna user, String namaBaru) async {
-    Map<String, dynamic> updateUser = {
-      'nama' : namaBaru,
-    };
-
-     QuerySnapshot querySnapshot = await _userCollection.where('idUser', isEqualTo: user.idUser).get();
-
-  // Iterasi melalui hasil pencarian
-  for (QueryDocumentSnapshot doc in querySnapshot.docs) {
-    // Mendapatkan id dokumen
-    String docId = doc.id;
-    // Memperbarui dokumen dengan id yang ditemukan
-    await _userCollection.doc(docId).update(updateUser);
+    });
   }
 
-}
-
-
- static Future<void> updateUserPhoto(Pengguna user) async {
+  static Future<void> updateUser(Pengguna user, String namaBaru) async {
     Map<String, dynamic> updateUser = {
-      'imageUrl' : user.imageUrl,
+      'nama': namaBaru,
     };
 
-     QuerySnapshot querySnapshot = await _userCollection.where('idUser', isEqualTo: user.idUser).get();
+    QuerySnapshot querySnapshot =
+        await _userCollection.where('idUser', isEqualTo: user.idUser).get();
 
-  // Iterasi melalui hasil pencarian
-  for (QueryDocumentSnapshot doc in querySnapshot.docs) {
-    // Mendapatkan id dokumen
-    String docId = doc.id;
-    // Memperbarui dokumen dengan id yang ditemukan
-    await _userCollection.doc(docId).update(updateUser);
+    // Iterasi melalui hasil pencarian
+    for (QueryDocumentSnapshot doc in querySnapshot.docs) {
+      // Mendapatkan id dokumen
+      String docId = doc.id;
+      // Memperbarui dokumen dengan id yang ditemukan
+      await _userCollection.doc(docId).update(updateUser);
+    }
   }
 
-}
+  static Future<void> updateUserPhoto(Pengguna user) async {
+    Map<String, dynamic> updateUser = {
+      'imageUrl': user.imageUrl,
+    };
 
-static Future<String?> uploadImage(XFile imageFile) async {
+    QuerySnapshot querySnapshot =
+        await _userCollection.where('idUser', isEqualTo: user.idUser).get();
+
+    // Iterasi melalui hasil pencarian
+    for (QueryDocumentSnapshot doc in querySnapshot.docs) {
+      // Mendapatkan id dokumen
+      String docId = doc.id;
+      // Memperbarui dokumen dengan id yang ditemukan
+      await _userCollection.doc(docId).update(updateUser);
+    }
+  }
+
+  static Future<String?> uploadImage(XFile imageFile) async {
     try {
       String fileName = path.basename(imageFile.path);
       Reference ref = _storage.ref().child('user/$fileName');
@@ -198,5 +236,4 @@ static Future<String?> uploadImage(XFile imageFile) async {
   }
 
   // ======================================================================================================
-
 }
